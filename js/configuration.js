25001cdf6cb531fa0946728028163d7b20462b10
Adriano <adri@EquipoDeTrabajo.(none)>
2013-11-13 10:47:45 +0100

    Modificacion con ofertas de empleo e identificacion de usuario

---
+++ js/configuration.js
@@ -0,0 +1,5 @@
+endPoint = 'http://localhost:8080/LMF-2.6.0/';
+endPointSOLR = 'http://localhost:8080/LMF-2.6.0/solr/jobs/select?json.nl=map&wt=json&rows=1000';
+endPointJSON = "http://localhost:8080/LMF-2.6.0/config/data/";
+endPointSemanticMatcher = "http://localhost:8080/gsimatch/rest/job/search?q=";
+endPointMahoutMatcher = "http://localhost:9090/RecomJob/JobMatch?user=";
